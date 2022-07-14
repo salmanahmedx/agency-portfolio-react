4a55a4eb1ed8b077ed4c5aca3afa0a5b84021514
Salman Ahmed <salmanahmedx@hotmail.com>
2022-07-14 17:31:22 +0600

    FAQ style, Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AboutUs from "./pages/AboutUs";
 import GlobalStyle from "./components/GlobalStyle";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyle></GlobalStyle>
+      <Nav />
       <AboutUs />
     </div>
   );
--- src/components/GlobalStyle.js
+++ src/components/GlobalStyle.js
@@ -44,6 +44,7 @@ p{
 h4{
     font-weight:bold;
     color: #23d997;
+    font-size: 2rem;
 }
 span{
     font-weight: bold;
