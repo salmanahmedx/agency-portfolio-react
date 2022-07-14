93c6c1c45fd7e73e54502232d88a940e10b2ea83
Salman Ahmed <salmanahmedx@hotmail.com>
2022-07-14 19:35:51 +0600

    movie detail component

--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -13,7 +13,6 @@ const MovieDetail = () => {
     useEffect(() => {
         const currentMovie = movies.filter((stateMovie) => stateMovie.id === id)
         setMovie(currentMovie)
-        console.log(movie)
     }, [id])
 
     return (
@@ -24,6 +23,12 @@ const MovieDetail = () => {
                         <h2>{movie[0]?.title}</h2>
                         <img src={movie[0]?.mainImg} alt={movie[0]?.title} />
                     </HeadLine>
+                    <Awards>
+                        {movie[0]?.awards?.map(award => <Award title={award.title} description={award.description} key={award.title}></Award>)}
+                    </Awards>
+                    <ImageDisplay>
+                        <img src={movie[0]?.secondaryImg} alt="" />
+                    </ImageDisplay>
                 </Details>
             )}
 
@@ -32,10 +37,70 @@ const MovieDetail = () => {
 };
 
 const Details = styled.div`
-
+color: white;
 `
 const HeadLine = styled.div`
-
+min-height: 90vh;
+padding-top:20vh;
+position: relative;
+h2{
+    position: absolute;
+    top: 10%;
+    left: 50%;
+    transform: translate(-50%, -10%);
+    img{
+        width: 100%;
+        height: 70vh;
+        object-fit: cover;
+    }
+}
 `
+
+const Awards = styled.div`
+min-height: 80vh;
+display: flex;
+margin: 5rem 10rem;
+align-items: center;
+justify-content: space-around;
+`
+const AwardStyle = styled.div`
+    padding: 5rem;
+    h3{
+        font-size:2rem;
+    }
+    .line{
+        width: 100%;
+        background: #23d997;
+        height: 0.5rem;
+        margin: 1rem 0rem;
+    }
+    p{
+        padding: 2rem 0rem;
+    }
+`
+const ImageDisplay = styled.div`
+min-height: 50vh;
+img{
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+}
+`
+
+
+const Award = ({ title, description }) => {
+    return (
+        <AwardStyle>
+            <h3>{title}</h3>
+            <div className="line"></div>
+            <p>{description}</p>
+        </AwardStyle>
+
+    )
+}
+
+
+
+
 
 export default MovieDetail;
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -12,19 +12,19 @@ const OurWork = () => {
             <Movie>
                 <h2>The Athlete</h2>
                 <div className="line"></div>
-                <Link to="">
+                <Link to="/work/the-athlete">
                     <img src={athlete} alt="athlete" /></Link>
             </Movie>
             <Movie>
                 <h2>The Racer</h2>
                 <div className="line"></div>
-                <Link to="">
+                <Link to="/work/the-racer">
                     <img src={theracer} alt="theracer" /></Link>
             </Movie>
             <Movie>
                 <h2>Good Times</h2>
                 <div className="line"></div>
-                <Link to="">
+                <Link to="/work/good-times">
                     <img src={goodtimes} alt="goodtimes" /></Link>
             </Movie>
         </Work>
