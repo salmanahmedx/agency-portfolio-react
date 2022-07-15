61899940dec5dc896e0c072df5480f7fe7a8a6d7
Salman Ahmed <salmanahmedx@hotmail.com>
2022-07-16 00:25:43 +0600

    About page animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,6 +2,7 @@ import React from 'react';
 import home1 from "../img/home1.png";
 import { About, Description, Image, Hide } from "../pages/styles";
 import { motion } from "framer-motion";
+import { titleAnim, fade, photoAnim } from '../pages/animation';
 
 
 const AboutSection = () => {
@@ -11,20 +12,20 @@ const AboutSection = () => {
             <Description>
                 <motion.div className="title" >
                     <Hide>
-                        <motion.h2 >We work to make</motion.h2>
+                        <motion.h2 variants={titleAnim}>We work to make</motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2 >your <span>dreams</span></motion.h2>
+                        <motion.h2 variants={titleAnim} >your <span>dreams</span></motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2 >come true</motion.h2>
+                        <motion.h2 variants={titleAnim} >come true</motion.h2>
                     </Hide>
                 </motion.div>
-                <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</p>
-                <button>Contact Us</button>
+                <motion.p variants={fade}>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</motion.p>
+                <motion.button variants={fade}>Contact Us</motion.button>
             </Description>
             <Image>
-                <img src={home1} alt="Camera Operator" />
+                <motion.img variants={photoAnim} src={home1} alt="Camera Operator" />
             </Image>
         </ About >
     );
--- src/pages/animation.js
+++ src/pages/animation.js
@@ -8,6 +8,7 @@ export const pageAnimation = {
         y: 0,
         transition: {
             duration: 1,
+            when: 'beforeChildren'
         },
     },
     exit: {
@@ -16,5 +17,36 @@ export const pageAnimation = {
         transition: {
             duration: 1,
         },
+    }
+}
+
+export const titleAnim = {
+    hidden: { y: 200 },
+    show: {
+        y: 0,
+        transition: { duration: 0.75, ease: "easeOut" }
+    }
+}
+
+export const fade = {
+    hidden: { opacity: 0 },
+    show: {
+        opacity: 1,
+        transition: {
+            ease: "easeOut",
+            duration: 0.75
+        }
+    }
+}
+
+export const photoAnim = {
+    hidden: { scale: 1.5, opacity: 0 },
+    show: {
+        scale: 1,
+        opacity: 1,
+        transition: {
+            ease: 'easeOut',
+            duration: 0.75
+        }
     }
 }
