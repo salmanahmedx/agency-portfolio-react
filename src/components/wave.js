9abae7bd529d2b06da3a57f66567334262414501
Salman Ahmed <salmanahmedx@hotmail.com>
2022-07-18 14:53:00 +0600

    page transition

--- src/components/wave.js
+++ src/components/wave.js
@@ -8,7 +8,7 @@ const wave = () => {
             <motion.path
                 initial={{ pathLength: 0, pathOffset: 1 }}
                 animate={{ pathLength: 1, pathOffset: 0 }} transition={{ duration: 1 }}
-                d="M1440 27.4774C1352.73 19.8184 1122.41 49.0556 899.331 227.276C620.48 450.052 354.282 355.647 170.328 185.318C23.165 49.0556 -4.21721 8.32998 0.487081 5" stroke="#D96ED4" strokeOpacity="0.1" strokeWidth="15" />
+                d="M1440 27.4774C1352.73 19.8184 1122.41 49.0556 899.331 227.276C620.48 450.052 354.282 355.647 170.328 185.318C23.165 49.0556 -4.21721 8.32998 0.487081 5" stroke="#D96ED4" strokeOpacity="0.25" strokeWidth="15" />
         </WaveSvg>
     );
 };
@@ -16,7 +16,7 @@ const wave = () => {
 const WaveSvg = styled.svg`
 position: absolute;
 left: 0;
-z-index: 1;
+z-index: 0;
 `
 
 export default wave;
--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -6,15 +6,26 @@ import { Link } from "react-router-dom";
 import styled from 'styled-components';
 import { motion } from "framer-motion";
 import { pageAnimation } from './animation';
+import { sliderContainer, titleAnim, fade, photoAnim, lineAnim, slider } from './animation';
+
 
 const OurWork = () => {
     return (
         <Work style={{ background: "#fff" }} exit="exit" variants={pageAnimation} initial="hidden" animate="show">
+            < motion.div variants={sliderContainer}>
+                <Frame1 variants={slider}></Frame1>
+                <Frame2 variants={slider}></Frame2>
+                <Frame3 variants={slider}></Frame3>
+                <Frame4 variants={slider}></Frame4>
+            </motion.div>
             <Movie>
-                <h2>The Athlete</h2>
-                <div className="line"></div>
+                <motion.h2 variants={fade}>The Athlete</motion.h2>
+                <motion.div className="line" variants={lineAnim}></motion.div>
                 <Link to="/work/the-athlete">
-                    <img src={athlete} alt="athlete" /></Link>
+                    <Hide>
+                        <motion.img src={athlete} variants={photoAnim} alt="athlete" />
+                    </Hide>
+                </Link>
             </Movie>
             <Movie>
                 <h2>The Racer</h2>
@@ -45,7 +56,7 @@ const Movie = styled.div`
 padding-bottom: 10rem;
 .line{
     height: 0.5rem;
-    background: #cccccc;
+    background: #23d997;
     margin-bottom:3rem;
 }
 img{
@@ -53,6 +64,29 @@ img{
     height: 70vh;
     object-fit: cover;
 }
+`
+const Hide = styled.div`
+overflow: hidden;
+`
+
+//Frame Animation
+const Frame1 = styled(motion.div)`
+position: fixed;
+left: 0;
+top: 10%;
+width: 100%;
+height: 100vh;
+background-color: #fffebf;
+z-index: 2;
+`
+const Frame2 = styled(Frame1)`
+background: #ff8efb;
+`
+const Frame3 = styled(Frame1)`
+background: #8ed2ff;
+`
+const Frame4 = styled(Frame1)`
+background: #8effa0;
 `
 
 export default OurWork;
