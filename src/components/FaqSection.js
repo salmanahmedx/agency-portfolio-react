5066e9da9681afc338efd9971deef85dbcba6d5d
Salman Ahmed <salmanahmedx@hotmail.com>
2022-07-18 15:57:50 +0600

    FAQ section toggle

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,44 +1,53 @@
 import React from 'react';
 import styled from 'styled-components';
-import { About } from "../pages/styles"
+import { About } from "../pages/styles";
+import Toggle from './Toggle';
+import { AnimateSharedLayout } from "framer-motion";
 
 
 const FaqSection = () => {
     return (
         <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
-            <div className="question">
-                <h4>How Do I Start?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, ipsa.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Daily Schedule</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, ipsa.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Different Payment Methods</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, ipsa.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>What products do you offer?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, ipsa.</p>
-                </div>
-            </div>
-        </Faq>
+            <AnimateSharedLayout>
+                <Toggle title="How Do I Start?">
+                    <div className="question">
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, ipsa.</p>
+                        </div>
+                    </div>
+                </Toggle>
+
+                <Toggle title="Daily Schedule">
+                    <div className="question">
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, ipsa.</p>
+                        </div>
+                    </div>
+                </Toggle>
+
+                <Toggle title="Different Payment Methods">
+                    <div className="question">
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, ipsa.</p>
+                        </div>
+                    </div>
+                </Toggle>
+
+                <Toggle title="What products do you offer?">
+                    <div className="question">
+
+                        <div className="answer">
+                            <p>Lorem ipsum dolor sit amet.</p>
+                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, ipsa.</p>
+                        </div>
+                    </div>
+                </Toggle>
+            </AnimateSharedLayout>
+        </Faq >
     );
 };
 
